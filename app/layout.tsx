3f20230498957c52f439f162c8497197801e5a75
xdesign678 <xdesign678@users.noreply.github.com>
2026-05-09 23:20:45 +0800

    feat(pwa): U1.7 add ios startup images

--- app/layout.tsx
+++ app/layout.tsx
@@ -37,6 +37,69 @@ const notoSerifSC = Noto_Serif_SC({
 
 const criticalCss = readFileSync(path.join(process.cwd(), 'app/globals-critical.css'), 'utf8');
 
+const IOS_STARTUP_IMAGES = [
+  {
+    url: '/icons/splash-640x1136.png',
+    media:
+      '(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)',
+  },
+  {
+    url: '/icons/splash-750x1334.png',
+    media:
+      '(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)',
+  },
+  {
+    url: '/icons/splash-828x1792.png',
+    media:
+      '(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)',
+  },
+  {
+    url: '/icons/splash-1125x2436.png',
+    media:
+      '(device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)',
+  },
+  {
+    url: '/icons/splash-1170x2532.png',
+    media:
+      '(device-width: 390px) and (device-height: 844px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)',
+  },
+  {
+    url: '/icons/splash-1179x2556.png',
+    media:
+      '(device-width: 393px) and (device-height: 852px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)',
+  },
+  {
+    url: '/icons/splash-1284x2778.png',
+    media:
+      '(device-width: 428px) and (device-height: 926px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)',
+  },
+  {
+    url: '/icons/splash-1290x2796.png',
+    media:
+      '(device-width: 430px) and (device-height: 932px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)',
+  },
+  {
+    url: '/icons/splash-1536x2048.png',
+    media:
+      '(device-width: 768px) and (device-height: 1024px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)',
+  },
+  {
+    url: '/icons/splash-1668x2224.png',
+    media:
+      '(device-width: 834px) and (device-height: 1112px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)',
+  },
+  {
+    url: '/icons/splash-1668x2388.png',
+    media:
+      '(device-width: 834px) and (device-height: 1194px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)',
+  },
+  {
+    url: '/icons/splash-2048x2732.png',
+    media:
+      '(device-width: 1024px) and (device-height: 1366px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)',
+  },
+];
+
 export const metadata: Metadata = {
   title: 'Compound · 让 AI 维护你的知识库',
   description:
@@ -46,6 +109,7 @@ export const metadata: Metadata = {
   appleWebApp: {
     capable: true,
     title: 'Compound',
+    startupImage: IOS_STARTUP_IMAGES,
     statusBarStyle: 'default',
   },
   icons: {
